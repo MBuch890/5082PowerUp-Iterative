2bbd4707150009331ff90a615fbd78f8da8c53a9
Mina Buchholz <mbuchholz890@gmail.com>
2018-01-23 18:58:29 -0600

    Adding prints, more auto setup
    Encoders, orientations, game message, and prints, as well as admin stuff

--- src/org/usfirst/frc/team5082/robot/Robot.java
+++ src/org/usfirst/frc/team5082/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import sun.nio.cs.ext.DoubleByte.Encoder;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,13 +22,16 @@ public class Robot extends IterativeRobot {
 	//VARIABLES
 	//0 is being used as a placeholder for port #s
 	
+	public static final int WHEEL_DIAMETER = 0;										//For calculating with encoders. Don't know which wheel will be encoded
+	public static final int PULSE_PER_REVOLUTION = 360;								//For calculating with encoders
+	
 	Spark mFrontLeft, mMidLeft, mBackLeft, mFrontRight, mMidRight, mBackRight;		//drive motors
 	DifferentialDrive drive;															//drive base w all drive motors
 	SpeedControllerGroup left, right;												//grouping motors by side
 	//Joystick joy;																	//joystick for 1 driver arcade drive
 	Timer timer;																		//game timer
-	DriverStation.Alliance alliance;													//what alliance are we? in enum
-	boolean isRed;																	//what alliance are we? in bool
+	Encoder encoder;																	//measuring the distance driven
+	Gyro gyro;																		//measuring the angle turned
 	
 	SendableChooser<Integer> chooser = new SendableChooser<Integer>();				//communicates what auto got chose, pt 1
 	
@@ -43,14 +47,14 @@ public class Robot extends IterativeRobot {
 		//INITIALIZING VARS
 		timer = new Timer();
 		//joy = new Joystick(0);
+		encoder = new Encoder(0, 0, false);
+		
 		mFrontLeft = new Spark(0);
 		mMidLeft = new Spark(0);
 		mBackLeft = new Spark(0);
 		mFrontRight = new Spark(0);
 		mMidRight = new Spark(0);
 		mBackRight = new Spark(0);
-		
-		color = DriverStation.getInstance().getAlliance;
 		
 		left = new SpeedControllerGroup(mFrontLeft, mMidLeft, mBackLeft);
 		right = new SpeedControllerGroup(mFrontRight, mMidRight, mBackRight);
@@ -59,6 +63,8 @@ public class Robot extends IterativeRobot {
 		auto = 0;
 		
 		//OTHER INIT STUFF
+		
+		encoder.setDistancePerPulse(Math.PI * WHEEL_DIAMETER / PULSE_PER_REVOLUTION);		//set multiplier for getDistance()
 		
 		drive.setSafetyEnabled(true);													//presumably this is useful, but we're not sure yet
 		
@@ -73,18 +79,14 @@ public class Robot extends IterativeRobot {
 	//This function is run once before autonomousPeriodic() begins
 	@Override
 	public void autonomousInit() {
-		
-		//What alliance r we?
-		if(alliance == DriverStation.Alliance.kBlue))
-			isRed = false;
-		else
-			isRed = true;
 																					
 		auto = chooser.getSelected();													//pick an auto to run
 		
-		//timer gets prepared to do nothing
+		//prep important sensors to go
 		timer.reset();
 		timer.start();
+		encoder.reset();
+		gyro.reset();
 	}
 
 	/**
@@ -93,42 +95,62 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void autonomousPeriodic() {
 		
-		//do different things depending on the auto that was chosen to run
-		switch (auto) {
+		double distance = encoder.get();
+		String orientation = (DriverStation.getGameSpecificMessage()).substring(0, 1);
 		
-			case 0: //default autonomous or no autonomous FINISHED
-				break;
+		if(DriverStation.isEnabled()) {
+		
+			//do different things depending on the auto that was chosen to run
+			switch (auto) {
+		
+				case 0: //default autonomous or no autonomous FINISHED
+					break;
 				
-			case 1: //left starting position
-				if(isRed) {
+				case 1: //left starting position
 					
-				}
-				else {
-					
-				}
-				break;
+					if (orientation.equalsIgnoreCase("L")) {
+						//TODO: drive forward
+					}
+					else if (orientation.equalsIgnoreCase("R")) {
+						//TODO: drive around to other side
+					}
+					else {
+						System.out.println("someone done diddly messed up my girl");
+					}
+					break;
 			
-			case 2: //center starting position
-				if(isRed) {
+				case 2: //center starting position
 					
-				}
-				else {
-					
-				}
-				break;
+					if (orientation.equalsIgnoreCase("L")) {
+						//TODO: drive forward
+					}
+					else if (orientation.equalsIgnoreCase("R")) {
+						//TODO: drive around to other side
+					}
+					else {
+						System.out.println("someone done diddly messed up my girl");
+					}
+					break;
 			
-			case 3: //right starting position
-				if (isRed) {
+				case 3: //right starting position
 					
-				}
-				else {
+					if (orientation.equalsIgnoreCase("L")) {
+						//TODO: drive forward
+					}
+					else if (orientation.equalsIgnoreCase("R")) {
+						//TODO: drive around to other side
+					}
+					else {
+						System.out.println("someone done diddly messed up my girl");
+					}
+					break;
+				
+				case 4: //just drive forward
 					
-				}
-				break;
+					//TODO: drive forward
+					break;
 				
-			case 4: //just drive forward
-				break;
-				
+			}
 		}
 	}
 
@@ -137,10 +159,17 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-		//Currently dead cause we dk what controller is gotta get used
-		//speed = joy.getRawAxis(1);
-		//rotation = joy.getRawAxis(4);
-		//drive.arcadeDrive(speed, rotation);
+		
+		if (DriverStation.isEnabled()) {
+			//Currently dead cause idk what controller is gotta get used
+			//speed = joy.getRawAxis(1);
+			//rotation = joy.getRawAxis(4);
+			//drive.arcadeDrive(speed, rotation);
+			
+			SmartDashboard.putDouble("Match Timer: ", 135 - timer.get());
+			SmartDashboard.putDouble("Your Orientation: ", gyro.getAngle());
+			
+		}
 	}
 
 	/**
